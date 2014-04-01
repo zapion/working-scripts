f1dd95d1b6102a92e8e124d2ad4b1e7351d07c79
pyang <pyang@Paul-Desktop.(none)>
2014-04-01 18:40:58 +0800

    finish enable debug certification app

--- enable_debug_CertApp.sh
+++ enable_debug_CertApp.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [ "$(whoami)" != "root" ]; then
+    echo "We might need to run as root permission"
+
 adb shell stop b2g
-#adb pull /data/b2g/mozilla/*.default/prefs.js
-#echo "user_pref('devtools.debugger.forbid-certified-apps', false);" >> prefs.js
-adb push prefs.js /data/b2g/mozilla/*.default/prefs.js
+location=`adb shell ls /data/b2g/mozilla | grep default`
+location="/data/b2g/mozilla/$location"
+location=`echo $location | tr -d '\r\n'`
+adb pull $location/prefs.js
+echo "user_pref('devtools.debugger.forbid-certified-apps', false);" >> prefs.js
+adb push prefs.js $location
 
 adb shell start b2g
-#rm prefs.js
+rm prefs.js
