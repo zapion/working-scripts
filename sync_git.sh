02a52d3199b8055280118fa97923dc2e8b3b940f
Paul Zapion Yang <zapionator@gmail.com>
2017-02-17 11:58:59 +0800

    add git to hg support

---
+++ sync_git.sh
@@ -0,0 +1,19 @@
+cd gecko-dev
+
+git format-patch HEAD~3 
+mv *.patch ..
+cd ..
+python git_to_hg.py *.patch
+cd gecko
+hg qpop -a
+hg qdelete awsy-lib
+hg qdelete awsy-package
+hg qdelete awsy-task
+hg import --no-commit ../*awsy-lib.patch
+hg qnew awsy-lib
+hg import --no-commit ../*awsy-package.patch
+hg qnew awsy-package
+hg import --no-commit ../*awsy-task.patch
+hg qnew awsy-task
+cd ..
+rm *.patch
